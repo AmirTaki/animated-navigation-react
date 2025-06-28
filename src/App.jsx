65a2bac3a0b105c586b3e9842765ec143868fcd7
amir <amirtaki623@gmail.com>
2025-06-28 21:50:59 +0330

    add navbar component at project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 
 import './App.css'
-
+import "./Nav/Navbar"
+import Navbar from './Nav/Navbar'
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <h1>hellow</h1>
+     <Navbar />
+
     </>
   )
 }
