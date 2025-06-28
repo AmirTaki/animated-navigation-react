775fa3defa5943c36a09c8846061952d5d94be4e
amir <amirtaki623@gmail.com>
2025-06-29 01:53:35 +0330

    add useState at project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
+import { useState } from 'react';
 import './App.css'
 import Navbar from './components/Navbar'
 import { CiMenuBurger } from "react-icons/ci";
 function App() {
-
+  const [showNav, setShowNav]  = useState(false);
+  
   return (
     <>
       <header>
