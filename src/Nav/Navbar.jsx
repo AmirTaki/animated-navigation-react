594c644d63424e2f3835696fa5538656cd0e7594
amir <amirtaki623@gmail.com>
2025-06-28 22:26:14 +0330

    add ul at project

--- src/Nav/Navbar.jsx
+++ src/Nav/Navbar.jsx
@@ -5,6 +5,16 @@ const Navbar = () => {
             <div>
                 <h2>Beauty</h2>
             </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Shop</li>
+                    <li>Blogs</li>
+                    <li>Pages</li>
+                    <li>Produc</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </div>
       </nav>
     )
