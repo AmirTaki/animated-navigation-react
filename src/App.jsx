a52e2757f3f45ec4d582098cd83dd4a07568ae75
amir <amirtaki623@gmail.com>
2025-06-29 02:08:27 +0330

    add home & about at project

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,26 @@ import { useState } from 'react';
 import './App.css'
 import Navbar from './components/Navbar'
 import { CiMenuBurger } from "react-icons/ci";
+import { BrowserRouter, Router, Switch, Route } from 'react-router-dom';
+import Home from "./pages/Home"
+import About from "./pages/About"
+
 function App() {
   const [showNav, setShowNav]  = useState(false);
 
   return (
     <>
-      <header>
-        <CiMenuBurger  onClick={()=> setShowNav(!showNav)}/>
-      </header>
-      {/* {showNav && <Navbar /> } */}
-      <Navbar show = {showNav} />
-  
+      <Router>
+        <header>
+          <CiMenuBurger  onClick={()=> setShowNav(!showNav)}/>
+        </header>
+        <Navbar show = {showNav} />
+        <div className='main'>
+          <Route path = "/" extact= {true} Component={Home}></Route>
+          <Route path = "/" extact= {true} Component={About}></Route>
+      
+        </div>
+      </Router>
     </>
   )
 }
