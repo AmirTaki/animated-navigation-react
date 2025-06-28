64c396204b1c589f4fd46e4ba05bfaf12704af03
amir <amirtaki623@gmail.com>
2025-06-28 23:57:30 +0330

    add active add sidenav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return(
-        <div className="sidenav">
-            <img src="" alt="logo"  className="logo"/>
+        <div className="sidenav active">
+            <img src="" alt="logo"  className="logo" />
             <ul>
                 <li><a href="/">Home</a></li>
             </ul>
